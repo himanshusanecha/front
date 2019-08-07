1b662ff066b1515bbdf55c078c24360de60cf455
Ben Hayward <ben@minds.com>
2019-08-07 15:00:30 +0100

    Added e2e test for registration

---
+++ cypress/integration/registration.spec.js
@@ -0,0 +1,80 @@
+context('Registration', () => {
+
+  const email = 'test@minds.com';
+  const password = 'Passw0rd!';
+  const noSymbolPass = 'Passw0rd';
+
+  const welcomeText = "Welcome to Minds!";
+  const passwordDontMatch = "Passwords must match.";
+  const passwordInvalid = " Password must have more than 8 characters. Including uppercase, numbers, special characters (ie. !,#,@), and cannot have spaces. ";
+
+  const usernameField = 'minds-form-register #username';
+  const emailField = 'minds-form-register #email';
+  const passwordField = 'minds-form-register #password';
+  const password2Field = 'minds-form-register #password2';
+  const checkbox = 'minds-form-register label:nth-child(2) .mdl-ripple--center';
+  const submitButton = 'minds-form-register .mdl-card__actions button';
+  const errorContainer = 'div:nth-child(2) > minds-form-register > div > div';
+
+  beforeEach(() => {
+    cy.visit('/login');
+    cy.location('pathname').should('eq', '/login');
+    cy.wait(1000)
+  });
+
+  it('should allow a user to register', () => {
+    //type values
+    cy.get(usernameField).focus().type(Math.random().toString(36).replace('0.', ''));
+    cy.get(emailField).focus().type(email);
+    cy.get(passwordField).focus().type(password);
+    cy.wait(500);
+
+    cy.get(password2Field).focus().type(password);
+    cy.get(checkbox).click();
+    
+    //submit
+    cy.get(submitButton).click();
+    cy.wait(5000);
+  
+    //onboarding modal shown
+    cy.get('m-onboarding--topics > div > h2:nth-child(1)')
+      .contains(welcomeText);
+  });
+
+  it('should display an error if password is invalid', () => {
+    
+    cy.get(usernameField).focus().type(Math.random().toString(36).replace('0.', ''));
+    cy.get(emailField).focus().type(email);
+    cy.get(passwordField).focus().type(noSymbolPass);
+    cy.wait(500);    
+    cy.get(password2Field).focus().type(noSymbolPass);
+    cy.get(checkbox).click();
+
+    //submit
+    cy.get(submitButton).click();
+    cy.wait(1000);
+
+    cy.scrollTo('top');
+    cy.get(errorContainer)
+      .contains(passwordInvalid);
+  });
+
+  it('should display an error if passwords do not match', () => {
+    cy.get(usernameField).focus().type(Math.random().toString(36).replace('0.', ''));
+    cy.get(emailField).focus().type(email);
+    cy.get('minds-form-register #password').focus().type(password);
+    cy.wait(500);
+  
+    cy.get(password2Field).focus().type(password + '!');
+    cy.get(checkbox).click();
+    
+    //submit
+    cy.get(submitButton).click();
+    cy.wait(1000);
+
+    cy.scrollTo('top');
+    cy.get(errorContainer)
+      .contains(passwordDontMatch);
+  });
+
+})
