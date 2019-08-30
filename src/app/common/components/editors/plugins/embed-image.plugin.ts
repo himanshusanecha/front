8439cd575acd46f79f40e98dda206a8743aba6d5
Ben Hayward <ben@minds.com>
2019-08-30 15:11:26 +0100

    Return void if there is no selected or deselected image

--- src/app/common/components/editors/plugins/embed-image.plugin.ts
+++ src/app/common/components/editors/plugins/embed-image.plugin.ts
@@ -150,6 +150,9 @@ export class EmbedImage {
     this.base.checkContentChanged();
   }
 
+  /**
+   * Event handler registration.
+   */
   public events() {
     /* prevent default image drag&drop */
     this.$element.addEventListener('dragover', e => {
@@ -227,8 +230,17 @@ export class EmbedImage {
     return data;
   }
 
+  /**
+   * On image select, responds to image click.
+   * @param { event }  e - event from DOM.
+   */
   public selectImage(e) {
     let $image = e.target;
+
+    if (!$image || $image.tagname) {
+      return;
+    }
+
     if ($image.tagName === 'SPAN') {
       $image = $image.parentNode.querySelector('img');
     }
@@ -254,6 +266,10 @@ export class EmbedImage {
     event.stopPropagation();
   }
 
+  /**
+   * On image deselect, called when image clicked away from.
+   * @param { event }  e - event from DOM.
+   */
   public unselectImage(e) {
     let $el = e.target,
       $image = document.querySelector('.medium-insert-image-active');
@@ -293,9 +309,6 @@ export class EmbedImage {
     this.$currentImage = null;
   }
 
-  /**
-   * Move caret at the beginning of the empty paragraph
-   */
   private moveCaret($el, position = 0) {
     let range, sel, el, textEl;
 
