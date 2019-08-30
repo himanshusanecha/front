c7ad718550b1c72ca6d16fc16fa5c2bf5332c884
Olivia Madrid <oliviajmadrid@gmail.com>
2019-08-30 06:43:55 -0400

    (feat): lint.

--- src/app/modules/blogs/card/card.ts
+++ src/app/modules/blogs/card/card.ts
@@ -2,11 +2,6 @@ import { Component } from '@angular/core';
 import { Session } from '../../../services/session';
 import { AttachmentService } from '../../../services/attachment';
 import { ACCESS } from '../../../services/list-options';
-// import { Router } from '@angular/router';
-// import { MediaModalComponent } from '../../media/modal/modal.component';
-// import { OverlayModalService } from '../../../services/ux/overlay-modal';
-// import { FeaturesService } from '../../../services/features.service';
-// import isMobile from '../../../helpers/is-mobile';
 
 @Component({
   moduleId: module.id,
@@ -19,13 +14,7 @@ export class BlogCard {
   blog;
   access = ACCESS;
 
-  constructor(
-    public session: Session,
-    public attachment: AttachmentService
-  ) // private router: Router,
-  // protected featuresService: FeaturesService,
-  // private overlayModal: OverlayModalService
-  {
+  constructor(public session: Session, public attachment: AttachmentService) {
     this.minds = window.Minds;
   }
 
@@ -34,29 +23,4 @@ export class BlogCard {
       value.thumbnail_src = 'assets/videos/earth-1/earth-1.png';
     this.blog = value;
   }
-
-  // showMediaModal() {
-  //   const route = this.blog.route
-  //     ? `/${this.blog.route}`
-  //     : `/blog/view${this.blog.guid}`;
-  //   const isNotTablet = Math.min(screen.width, screen.height) < 768;
-
-  //   if (isMobile() && isNotTablet) {
-  //     this.router.navigate([route]);
-  //     return;
-  //   }
-
-  //   if (!this.featuresService.has('media-modal')) {
-  //     this.router.navigate([route]);
-  //     return;
-  //   } else {
-  //     this.blog.modal_source_url = this.router.url;
-
-  //     this.overlayModal
-  //       .create(MediaModalComponent, this.blog, {
-  //         class: 'm-overlayModal--media',
-  //       })
-  //       .present();
-  //   }
-  // }
 }
