cbabacd10cd7a4d2e3cd38fde88ce79eff7916b6
Ben Hayward <ben@minds.com>
2019-09-02 15:37:01 +0100

    Updated for feedback

--- src/app/common/components/editors/plugins/embed-image.plugin.ts
+++ src/app/common/components/editors/plugins/embed-image.plugin.ts
@@ -237,7 +237,7 @@ export class EmbedImage {
   public selectImage(e) {
     let $image = e.target;
 
-    if (!$image || $image.tagname) {
+    if (!$image || !$image.tagname) {
       return;
     }
 
@@ -274,7 +274,7 @@ export class EmbedImage {
     let $el = e.target,
       $image = document.querySelector('.medium-insert-image-active');
 
-    if (!$image) {
+    if (!$el || !$el.tagname) {
       return;
     }
 
