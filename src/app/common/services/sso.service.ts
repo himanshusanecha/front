0c98f7a6212d54b58303e3a5da326394dad47843
Emiliano Balbuena <edgebal@gmail.com>
2019-11-14 13:56:46 -0300

    (feat): SSO flow upon page initialization

--- src/app/common/services/sso.service.ts
+++ src/app/common/services/sso.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@angular/core';
 import { SiteService } from './site.service';
 import { Client } from '../../services/api/client';
+import { Session } from '../../services/session';
 
 @Injectable()
 export class SsoService {
   protected readonly minds = window.Minds;
 
-  constructor(protected site: SiteService, protected client: Client) {}
+  constructor(
+    protected site: SiteService,
+    protected client: Client,
+    protected session: Session
+  ) {}
 
   isRequired(): boolean {
     return this.site.isProDomain;
@@ -14,9 +19,22 @@ export class SsoService {
 
   async connect() {
     try {
-      const response: any = await this.client.postRaw(
+      const connect: any = await this.client.postRaw(
         `${this.minds.site_url}api/v2/sso/connect`
       );
+
+      if (connect && connect.status === 'success') {
+        // TODO: Use headers
+        const authorization: any = await this.client.post(
+          'api/v2/sso/authorize',
+          {
+            token: connect.token,
+          }
+        );
+
+        window.Minds.user = authorization.user;
+        this.session.login(window.Minds.user);
+      }
     } catch (e) {
       console.error(e);
     }
