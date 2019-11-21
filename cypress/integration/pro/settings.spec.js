ae1aa63f7e9808951d35e523c7b3943639ec5140
Olivia Madrid <oliviajmadrid@gmail.com>
2019-11-21 00:29:28 -0700

    (e2e): pro settings

--- cypress/integration/pro/settings.spec.js
+++ cypress/integration/pro/settings.spec.js
@@ -5,49 +5,91 @@
 context('Pro Settings', () => {
   if (Cypress.env().pro_password) {
     // required to run tests against pro user only.
-    const title = '#title';
-    const headline = '#headline';
     const activityContainer = 'minds-activity';
-    const footerText = '#footer_text';
+
+    function data(str) {
+      return `[data-minds=${str}]`;
+    }
+
+    const general = {
+      title: data('title'),
+      headline: data('headline'),
+      publish: data('publish'),
+      strings: {
+        title: 'Minds Pro E2E',
+        headline: 'This headline is a test',
+      },
+    };
 
     const theme = {
-      textColor: '#textColor',
-      primaryColor: '#primaryColor',
-      plainBackgroundColor: '#plainBgColor',
-      schemeLight: '#scheme_light',
-      schemeDark: '#scheme_dark',
+      textColor: data('textColor'),
+      // textColorPicker: data('textColorPicker'),
+      primaryColor: data('primaryColor'),
+      // primaryColorPicker: data('primaryColorPicker'),
+      plainBgColor: data('plainBgColor'),
+      // plainBgColorPicker: data('plainBgColorPicker'),
+      schemeLight: data('schemeLight'),
+      schemeDark: data('schemeDark'),
       aspectRatio: {
-        169: '#tile_ratio_16:9', //  16:9
-        1610: '#tile_ratio_16:10', // 16:10
-        43: '#tile_ratio_4:3', // 4:3
-        11: '#tile_ratio_1:1', // 1:1
+        169: data('tileRatio_16:9'), //  16:9
+        1610: data('tileRatio_16:10'), // 16:10
+        43: data('tileRatio_4:3'), // 4:3
+        11: data('tileRatio_1:1'), // 1:1
+      },
+      strings: {
+        textColor: '#4690df',
+        primaryColor: '#cb7848',
+        plainBgColor: '#b4bbf0',
+        textColorRgb: 'rgb(70, 144, 223)',
+        primaryColorRgb: 'rgb(203, 120, 72)',
+        plainBgColorRgba: 'rgba(180, 187, 240, 0.627)',
+      },
+    };
+
+    //TODOOJM
+    const assets = {
+      logo: data('logo'),
+      background: data('background'),
+      strings: {
+        logoFixture: '../../fixtures/avatar.jpg',
+        backgroundFixture:
+          '../../fixtures/international-space-station-1776401_1920.jpg',
       },
     };
 
     const hashtags = {
-      labelInput0: '#tag-label-0',
-      hashtagInput0: '#tag-tag-0',
-      labelInput1: '#tag-label-1',
-      hashtagInput1: '#tag-tag-1',
-      label1: 'label1',
-      label2: 'label2',
-      label3: 'label3',
-      hashtag1: '#hashtag1',
-      hashtag2: '#hashtag2',
-      hashtag3: '#hashtag3',
+      add: data('addHashtag'),
+      label0: data('tag__label--0'),
+      tag0: data('tag__tag--0'),
+      label1: data('tag__label--1'),
+      tag1: data('tag__tag--1'),
+      strings: {
+        label0: 'myLabel0',
+        label1: 'myLabel1',
+        tag0: '#hashtag0',
+        tag1: '#hashtag1',
+      },
     };
 
     const footer = {
-      hrefInput: `#footer_link-href-0`,
-      titleInput: `#footer_link-title-0`,
+      text: data('footerText'),
+      add: data('addFooterLink'),
+      linkTitle: data('footerLink__title--0'),
+      linkHref: data('footerLink__href--0'),
+      strings: {
+        text: 'This is a footer',
+        linkTitle: 'Minds',
+        linkHref: 'https://www.minds.com/',
+      },
     };
 
-    const strings = {
-      title: 'Minds Pro E2E',
-      headline: 'This headline is a test',
-      footer: 'This is a footer',
-      footerTitle: 'Minds',
-      footerHref: 'https://www.minds.com/',
+    // TODO
+    // const domain = {};
+
+    const payouts = {
+      strings: {
+        method: 'USD',
+      },
     };
 
     before(() => {
@@ -55,7 +97,6 @@ context('Pro Settings', () => {
     });
 
     after(() => {
-      // cy.visit(`/${Cypress.env().username}`);
       cy.visit('/pro/' + Cypress.env().pro_username + '/settings/hashtags')
         .location('pathname')
         .should(
@@ -80,25 +121,82 @@ context('Pro Settings', () => {
 
     it('should update the title and headline', () => {
       //enter data
-      cy.get(title)
+      cy.get(general.title)
         .focus()
         .clear()
-        .type(strings.title);
+        .type(general.strings.title);
 
-      cy.get(headline)
+      cy.get(general.headline)
         .focus()
         .clear()
-        .type(strings.headline);
+        .type(general.strings.headline);
 
       saveAndPreview();
       //check tab title.
       cy.title().should(
         'eq',
-        strings.title + ' - ' + strings.headline + ' | Minds'
+        general.strings.title + ' - ' + general.strings.headline + ' | Minds'
       );
     });
 
-    // Need to find a way around the color input in Cypress.
+    // it('should enable pro theme', () => {
+    // cy.get(general.publish).check();
+
+    // saveAndPreview();
+
+    // });
+
+    // it('should disable pro theme', () => {
+    // cy.get(general.publish).uncheck();
+
+    // saveAndPreview();
+
+    // });
+
+    it('should allow the user to set theme colors', () => {
+      cy.contains('Theme').click();
+
+      cy.get(theme.textColor)
+        .click()
+        .clear()
+        .type(theme.strings.textColor);
+
+      cy.get(theme.primaryColor)
+        .click()
+        .clear()
+        .type(theme.strings.primaryColor);
+
+      cy.get(theme.plainBgColor)
+        .click()
+        .clear()
+        .type(theme.strings.plainBgColor);
+
+      saveAndPreview();
+
+      cy.contains('feed')
+        .should('have.css', 'color')
+        .and('eq', theme.strings.textColorRgb);
+
+      cy.get('.m-pro__searchBox input').should(
+        'have.css',
+        'background-color',
+        theme.strings.plainBgColorRgba
+      );
+
+      cy.contains('feed').should(
+        'have.css',
+        'color',
+        theme.strings.textColorRgb
+      );
+      // .and('eq', theme.strings.textColorRgb);
+
+      cy.contains('feed').click();
+      cy.get('.m-proHamburgerMenu__trigger')
+        .click()
+        .get('.m-proHamburgerMenu__item--active')
+        .should('have.css', 'color')
+        .and('eq', theme.strings.primaryColorRgb);
+    });
 
     it('should allow the user to set a dark theme for posts', () => {
       cy.contains('Theme').click();
@@ -128,60 +226,86 @@ context('Pro Settings', () => {
         .and('eq', 'rgb(255, 255, 255)');
     });
 
+    it('should allow the user to upload logo and background images', () => {
+      cy.uploadFile(assets.logo, assets.strings.logoFixture, 'image/jpg');
+      // // upload the image
+      // cy.uploadFile('.medium-media-file-input', '../fixtures/international-space-station-1776401_1920.jpg', 'image/jpg')
+      //   .wait('@postMedia').then(xhr => {
+      //     expect(xhr.status).to.equal(200);
+      //     expect(xhr.response.body.status).to.equal('success');
+      //   });
+
+      cy.uploadFile(
+        assets.background,
+        assets.strings.backgroundFixture,
+        'image/jpg'
+      );
+    });
+
     it('should allow the user to set category hashtags', () => {
       cy.contains('Hashtags').click();
 
-      cy.contains('Add').click();
+      cy.get(hashtags.add).click();
 
-      cy.get(hashtags.labelInput0)
+      cy.get(hashtags.label0)
         .clear()
-        .type(hashtags.label1);
+        .type(hashtags.strings.label0);
 
-      cy.get(hashtags.hashtagInput0)
+      cy.get(hashtags.tag0)
         .clear()
-        .type(hashtags.hashtag1);
+        .type(hashtags.strings.tag0);
 
-      cy.contains('Add').click();
+      cy.get(hashtags.add).click();
 
-      cy.get(hashtags.labelInput1)
-        .first()
+      cy.get(hashtags.label1)
         .clear()
-        .type(hashtags.label2);
+        .type(hashtags.strings.label1);
 
-      cy.get(hashtags.hashtagInput1)
-        .first()
+      cy.get(hashtags.tag1)
         .clear()
-        .type(hashtags.hashtag2);
+        .type(hashtags.strings.tag1);
 
       saveAndPreview();
 
       //check the labels are present and clickable.
-      cy.contains('label1');
-      cy.contains('label2');
+      cy.contains(hashtags.strings.label1);
+      cy.contains(hashtags.strings.label2);
     });
 
     it('should allow the user to set footer', () => {
       cy.contains('Footer').click();
 
-      cy.get(footerText)
-        .clear()
-        .type(strings.footer);
+      // add a new footer link
+      cy.get(footer.add).click();
 
-      cy.contains('Add Link').click();
-
-      cy.get(footer.hrefInput)
+      cy.get(footer.linkHref)
         .clear()
-        .type(strings.footerHref);
+        .type(footer.strings.linkHref);
 
-      cy.get(footer.titleInput)
+      cy.get(footer.linkTitle)
         .clear()
-        .type(strings.footerTitle);
+        .type(footer.strings.linkTitle);
+
+      // add footer text
+      cy.get(footer.text)
+        .clear()
+        .type(footer.strings.text);
 
       saveAndPreview();
 
-      cy.contains(strings.footerTitle)
+      cy.contains(footer.strings.footerTitle)
         .should('have.attr', 'href')
-        .should('contain', strings.footerHref);
+        .should('contain', footer.strings.footerHref);
+
+      cy.get(footer.text).should('contain', footer.strings.text);
+    });
+
+    it('should allow the user to set payout method', () => {
+      cy.contains('Payouts').click();
+
+      cy.contains(payouts.method).check();
+
+      // session.getLoggedInUser().merchant.service: "stripe"
     });
 
     //save, await response, preview.
@@ -202,7 +326,7 @@ context('Pro Settings', () => {
     function clearHashtags() {
       cy.contains('Hashtags').click();
 
-      cy.contains('Add').click();
+      cy.get(hashtags.add).click();
 
       cy.contains('clear').click({ multiple: true });
       saveAndPreview();
@@ -214,7 +338,7 @@ context('Pro Settings', () => {
     //   cy.contains('Theme')
     //     .click();
 
-    //   cy.get(theme.plainBackgroundColor).then(elem => {
+    //   cy.get(theme.plainBgColor).then(elem => {
     //     elem.val('#00dd00');
     //         //save and await response
     //     cy.contains('Save')
