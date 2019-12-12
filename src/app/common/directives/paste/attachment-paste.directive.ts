c0eeae284d4de1722a113ab9adfd32f843c72584
Marcelo Rivera <malrogf@gmail.com>
2019-12-12 11:41:35 -0300

    (fix): remove aliases and use correct naming for outputs

--- src/app/common/directives/paste/attachment-paste.directive.ts
+++ src/app/common/directives/paste/attachment-paste.directive.ts
@@ -2,8 +2,8 @@ import { Directive, EventEmitter, HostListener, Output } from '@angular/core';
 
 @Directive({ selector: '[m-attachment-paste]' })
 export class AttachmentPasteDirective {
-  @Output('onFilePaste')
-  onFilePaste: EventEmitter<File> = new EventEmitter<File>();
+  @Output()
+  filePaste: EventEmitter<File> = new EventEmitter<File>();
 
   private focused: boolean = false;
 
@@ -17,11 +17,11 @@ export class AttachmentPasteDirective {
 
   @HostListener('window:paste', ['$event']) onPaste(event: ClipboardEvent) {
     if (this.focused) {
-      for (let index in event.clipboardData.items) {
+      for (const index in event.clipboardData.items) {
         const item: DataTransferItem = event.clipboardData.items[index];
 
         if (item.kind === 'file') {
-          this.onFilePaste.emit(item.getAsFile());
+          this.filePaste.emit(item.getAsFile());
           break;
         }
       }
