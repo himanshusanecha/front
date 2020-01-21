ce381393163aa78e1a3eee4f8d5f43be2d87acb2
Marcelo Rivera <malrogf@gmail.com>
2020-01-21 09:37:16 -0300

    (fix): ios safari issue (fix): long username overflow in notice step (fix): don't redirect out of onboarding (fix): add a bit of margin to onboarding

--- src/app/modules/auth/register.component.ts
+++ src/app/modules/auth/register.component.ts
@@ -14,6 +14,7 @@ import { MindsTitle } from '../../services/ux/title';
 import { FeaturesService } from '../../services/features.service';
 import { V2TopbarService } from '../../common/layout/v2-topbar/v2-topbar.service';
 import { OnboardingV2Service } from '../onboarding-v2/service/onboarding.service';
+import { iOSVersion } from '../../helpers/is-safari';
 
 @Component({
   selector: 'm-register',
@@ -29,6 +30,9 @@ export class RegisterComponent implements OnInit, OnDestroy {
   referrer: string;
   @HostBinding('class.m-register__newDesign')
   newDesign: boolean = false;
+  @HostBinding('class.m-register__iosFallback')
+  iosFallback: boolean = false;
+
   private redirectTo: string;
 
   flags = {
@@ -58,9 +62,9 @@ export class RegisterComponent implements OnInit, OnDestroy {
     }
 
     this.newDesign = this.featuresService.has('register_pages-december-2019');
-
     if (this.newDesign) {
       this.topbarService.toggleVisibility(false);
+      this.iosFallback = iOSVersion() !== null;
     }
   }
 
