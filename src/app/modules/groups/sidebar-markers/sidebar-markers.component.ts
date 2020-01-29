2738e10c73b21af1fba231a89cccf8d3ce38e3c6
Marcelo Rivera <malrogf@gmail.com>
2020-01-29 16:33:47 -0300

    (fix): don't duplicate groups in the sidebar

--- src/app/modules/groups/sidebar-markers/sidebar-markers.component.ts
+++ src/app/modules/groups/sidebar-markers/sidebar-markers.component.ts
@@ -4,6 +4,8 @@ import {
   ViewChild,
   ChangeDetectorRef,
   HostListener,
+  OnInit,
+  OnDestroy,
 } from '@angular/core';
 import { interval, timer } from 'rxjs';
 import { startWith, map, tap, throttle } from 'rxjs/operators';
@@ -17,7 +19,7 @@ import { GroupsService } from '../groups-service';
   selector: 'm-group--sidebar-markers',
   templateUrl: 'sidebar-markers.component.html',
 })
-export class GroupsSidebarMarkersComponent {
+export class GroupsSidebarMarkersComponent implements OnInit, OnDestroy {
   inProgress: boolean = false;
   $updateMarker;
   markers = [];
@@ -52,6 +54,10 @@ export class GroupsSidebarMarkersComponent {
         return;
       }
       if (update.show) {
+        // if the group already exists in the list, don't re-add it
+        if (this.groups.findIndex(g => g.guid == update.guid) !== -1) {
+          return;
+        }
         this.groupsService.load(update.guid).then(group => {
           this.groups.unshift(group);
         });
