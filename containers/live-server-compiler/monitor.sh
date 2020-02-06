ba9aef3f87af8b2082401dc668b4fb68a77ab9a1
Emiliano Balbuena <edgebal@gmail.com>
2020-02-06 15:02:30 -0300

    (feat): Change deploy url

--- containers/live-server-compiler/monitor.sh
+++ containers/live-server-compiler/monitor.sh
@@ -2,4 +2,4 @@
 set -e
 
 cd /var/www/Minds/front
-npx nodemon -L --delay 3 --watch dist --ignore dist/server/ --ignore dist/server.js --ext js,css,jpg,png,svg,mp4,webp,webm  --exec "/usr/bin/env sh" /build.sh
+npx nodemon -L --delay 3 --watch server.ts --watch dist --ignore dist/server/ --ignore dist/server.js --ext js,css,jpg,png,svg,mp4,webp,webm  --exec "/usr/bin/env sh" /build.sh
