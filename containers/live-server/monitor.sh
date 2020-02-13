23b0779e5df3a981a4e7f04891465aa56a6f8178
Ben Hayward <ben@minds.com>
2020-02-13 14:51:58 +0000

    Reverting local dev stack change

--- containers/live-server/monitor.sh
+++ containers/live-server/monitor.sh
@@ -2,8 +2,4 @@
 set -e
 
 cd /var/www/Minds/front
-<<<<<<< HEAD
-npx nodemon -L --delay 3 --watch dist --ignore dist/en --ext js,mjs  --exec "/usr/bin/env sh" /serve.sh
-=======
 npx nodemon --delay 3 --watch dist/server.js --watch dist/server --ext js,mjs  --exec "/usr/bin/env sh" /serve.sh
->>>>>>> 745769a53afa1c02ba6513b265ede87a162a0515
