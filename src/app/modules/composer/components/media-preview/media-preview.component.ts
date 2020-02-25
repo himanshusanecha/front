b0772323f62b9a43f1a7cff988b66036b84d8abf
Emi Balbuena <edgebal@gmail.com>
2020-02-25 13:44:28 -0300

    (chore): Portrait mode for image and video preview

--- src/app/modules/composer/components/media-preview/media-preview.component.ts
+++ src/app/modules/composer/components/media-preview/media-preview.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  Input,
+} from '@angular/core';
 import { PreviewResource } from '../../composer.service';
 import { DomSanitizer } from '@angular/platform-browser';
 
@@ -8,15 +13,54 @@ import { DomSanitizer } from '@angular/platform-browser';
   templateUrl: 'media-preview.component.html',
 })
 export class MediaPreviewComponent {
-  @Input() previewResource: PreviewResource;
+  @Input('previewResource') set _previewResource(
+    previewResource: PreviewResource
+  ) {
+    this.portrait = false;
+    this.previewResource = previewResource;
+  }
 
-  constructor(protected domSanitizer: DomSanitizer) {}
+  previewResource: PreviewResource;
+
+  portrait: boolean = false;
+
+  constructor(
+    protected domSanitizer: DomSanitizer,
+    protected cd: ChangeDetectorRef
+  ) {}
 
   trustedUrl(url: string) {
     return this.domSanitizer.bypassSecurityTrustUrl(url);
   }
 
   fitForImage(image: HTMLImageElement) {
-    //console.log(image);
+    if (!image) {
+      return false;
+    }
+
+    const currentValue = this.portrait;
+    this.portrait = image.naturalHeight >= image.naturalWidth;
+
+    if (this.portrait !== currentValue) {
+      this.detectChanges();
+    }
+  }
+
+  fitForVideo(video: HTMLVideoElement) {
+    if (!video) {
+      return false;
+    }
+
+    const currentValue = this.portrait;
+    this.portrait = video.videoHeight >= video.videoWidth;
+
+    if (this.portrait !== currentValue) {
+      this.detectChanges();
+    }
+  }
+
+  detectChanges() {
+    this.cd.detectChanges();
+    this.cd.markForCheck();
   }
 }
