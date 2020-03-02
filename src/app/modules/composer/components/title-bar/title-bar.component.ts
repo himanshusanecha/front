c473515abb27bd8098cf4a701b026950824a509e
Emi Balbuena <edgebal@gmail.com>
2020-03-02 07:11:14 -0300

    (chore): JSDocs

--- src/app/modules/composer/components/title-bar/title-bar.component.ts
+++ src/app/modules/composer/components/title-bar/title-bar.component.ts
@@ -7,28 +7,53 @@ import {
 } from '@angular/core';
 import { ACCESS, LICENSES } from '../../../../services/list-options';
 
+/**
+ * Composer title bar component. It features a label and a dropdown menu
+ * with not-that-important options.
+ */
 @Component({
   selector: 'm-composer__titleBar',
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: 'title-bar.component.html',
 })
 export class TitleBarComponent {
+  /**
+   * Composer textarea ID
+   */
   @Input() id: string;
 
+  /**
+   * Current visibility (access ID) value
+   */
   @Input() visibility: string;
 
+  /**
+   * Current license value
+   */
   @Input() license: string;
 
+  /**
+   * Can we change visibility? (disabled on groups)
+   */
   @Input() canChangeVisibility: boolean = true;
 
+  /**
+   * Visibility emitter
+   */
   @Output('onVisibility') onVisibilityEmitter: EventEmitter<
     string
   > = new EventEmitter<string>();
 
+  /**
+   * License emitter
+   */
   @Output('onLicense') onLicenseEmitter: EventEmitter<
     string
   > = new EventEmitter<string>();
 
+  /**
+   * Visibility items list
+   */
   visibilityItems: Array<{ text: string; value: string }> = ACCESS.map(
     ({ text, value }) => ({
       text,
@@ -36,8 +61,15 @@ export class TitleBarComponent {
     })
   );
 
+  /**
+   * License items list
+   */
   licenseItems: Array<{ text: string; value: string }> = LICENSES;
 
+  /**
+   * Sends the visibility changed emitter
+   * @param $event
+   */
   onVisibilityClick($event) {
     if (!this.canChangeVisibility) {
       return;
@@ -46,6 +78,10 @@ export class TitleBarComponent {
     this.onVisibilityEmitter.emit($event);
   }
 
+  /**
+   * Sends the visibility license emitter
+   * @param $event
+   */
   onLicenseClick($event) {
     this.onLicenseEmitter.emit($event);
   }
