5ad289ea8f9fdec0ea9ded10007d14e919fae863
Emi Balbuena <edgebal@gmail.com>
2020-03-11 16:54:31 -0300

    (fix): Wrong workaround push

--- src/app/common/services/configs.service.ts
+++ src/app/common/services/configs.service.ts
@@ -22,7 +22,6 @@ export class ConfigsService {
         `api/v1/minds/config${this.queryString}`
       );
       this.isReady$.next(true);
-      (<any>this.configs).features['activity-composer'] = true;
       this.redirectToRootIfInvalidDomain();
     } catch (err) {
       console.error(err);
