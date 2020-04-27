943198302de340f2952d47e8799eb925fefe0306
Marcelo Rivera <malrogf@gmail.com>
2020-04-27 12:00:34 -0300

    (chore): add return types

--- src/app/common/layout/v3-topbar/user-menu/user-menu.component.ts
+++ src/app/common/layout/v3-topbar/user-menu/user-menu.component.ts
@@ -10,7 +10,7 @@ import { Session } from '../../../../services/session';
 import { ThemeService } from '../../../../common/services/theme.service';
 import { Subscription } from 'rxjs';
 import { Navigation as NavigationService } from '../../../../services/navigation';
-import { RouterLink } from '@angular/router';
+import { MindsUser } from '../../../../interfaces/entities';
 
 @Component({
   selector: 'm-usermenu__v3',
@@ -47,39 +47,39 @@ export class UserMenuV3Component implements OnInit, OnDestroy {
     private themeService: ThemeService
   ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.session.isLoggedIn(() => this.detectChanges());
     this.themeSubscription = this.themeService.isDark$.subscribe(
       isDark => (this.isDark = isDark)
     );
   }
 
-  getCurrentUser() {
+  getCurrentUser(): MindsUser {
     return this.session.getLoggedInUser();
   }
 
-  isAdmin() {
+  isAdmin(): boolean {
     return this.session.isAdmin();
   }
 
-  toggleMenu() {
+  toggleMenu(): void {
     this.isOpen = !this.isOpen;
   }
 
-  closeMenu() {
+  closeMenu(): void {
     this.isOpen = false;
   }
 
-  detectChanges() {
+  detectChanges(): void {
     this.cd.markForCheck();
     this.cd.detectChanges();
   }
 
-  toggleTheme() {
+  toggleTheme(): void {
     this.themeService.toggleUserThemePreference();
   }
 
-  toggleFooterLinks() {
+  toggleFooterLinks(): void {
     if (this.maxFooterLinks === 5) {
       this.maxFooterLinks = Infinity;
     } else {
@@ -87,7 +87,7 @@ export class UserMenuV3Component implements OnInit, OnDestroy {
     }
   }
 
-  ngOnDestroy() {
+  ngOnDestroy(): void {
     this.themeSubscription.unsubscribe();
   }
 }
