b5cee8435ab7988d38941d5a78618473d82ee054
Emi Balbuena <edgebal@gmail.com>
2020-05-05 18:31:14 -0300

    (chore): Explore shop link

--- src/app/modules/wire/v2/creator/shop/shop.component.ts
+++ src/app/modules/wire/v2/creator/shop/shop.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Output,
+} from '@angular/core';
 import { WireV2Service } from '../../wire-v2.service';
 import { ShopService } from './shop.service';
 
@@ -13,9 +18,23 @@ import { ShopService } from './shop.service';
 })
 export class WireCreatorShopComponent {
   /**
+   * Dismiss intent event emitter
+   */
+  @Output('onDismissIntent') dismissIntentEmitter: EventEmitter<
+    void
+  > = new EventEmitter<void>();
+
+  /**
    * Constructor
    * @param service
    * @param shop
    */
-  constructor(public service: WireV2Service, protected shop: ShopService) {}
+  constructor(public service: WireV2Service, public shop: ShopService) {}
+
+  /**
+   * Triggers the dismiss modal event
+   */
+  dismiss() {
+    this.dismissIntentEmitter.emit();
+  }
 }
