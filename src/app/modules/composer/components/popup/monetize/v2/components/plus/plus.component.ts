f795f67222494181412e23d889d4970ac50b9504
Olivia Madrid <oliviajmadrid@gmail.com>
2020-06-10 18:21:35 -0600

    make isPro isPlus active check async

--- src/app/modules/composer/components/popup/monetize/v2/components/plus/plus.component.ts
+++ src/app/modules/composer/components/popup/monetize/v2/components/plus/plus.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ProService } from '../../../../../../../pro/pro.service';
 import { WirePaymentHandlersService } from '../../../../../../../wire/wire-payment-handlers.service';
 import { WireModalService } from '../../../../../../../wire/wire-modal.service';
+import { WireEventType } from '../../../../../../../wire/v2/wire-v2.service';
+import { Session } from '../../../../../../../../services/session';
+import { DynamicModalSettings } from '../../../../../../../../common/components/stackable-modal/stackable-modal.component';
+import { PopupService } from '../../../../popup.service';
 
 @Component({
   selector: 'm-composer__monetizeV2__plus',
@@ -9,30 +13,49 @@ import { WireModalService } from '../../../../../../../wire/wire-modal.service';
 })
 export class ComposerMonetizeV2PlusComponent implements OnInit {
   isPro;
+  stackableModalSettings: DynamicModalSettings;
+
   constructor(
     private proService: ProService,
     private wirePaymentHandlers: WirePaymentHandlersService,
-    private wireModal: WireModalService
+    private wireModal: WireModalService,
+    private session: Session,
+    private popup: PopupService
   ) {}
 
   ngOnInit(): void {
-    this.isPro = this.proService.isActive();
+    this.setup();
   }
 
-  showProModal(): void {
-    // TODOOJM enable after upgrade modal MR is merged
-    // const plusGuid = await this.wirePaymentHandlers.get('plus');
-    // const wireEvent = await this.wireModal
-    //   .present(plusGuid, {
-    //     default: {
-    //       type: 'money',
-    //       upgradeType: 'pro',
-    //     },
-    //   })
-    //   .toPromise();
-    // if (wireEvent.type === WireEventType.Completed) {
-    //   // const wire = wireEvent.payload;
-    //   this.isPro = true;
-    // }
+  async setup(): Promise<void> {
+    this.isPro = await this.proService.isActive();
+  }
+
+  // openShareModal(): void {
+  //   const componentClass = ShareModalComponent,
+  //     data = this.site.baseUrl + this.pageUrl.substr(1),
+  //     opts = { class: 'm-overlayModal__share' };
+
+  //   this.stackableModalSettings = {
+  //     componentClass: componentClass,
+  //     data: data,
+  //     opts: opts,
+  //   };
+  // }
+
+  async openProUpgradeModal(): Promise<void> {
+    const plusGuid = await this.wirePaymentHandlers.get('pro');
+    const wireEvent = await this.wireModal
+      .present(plusGuid, {
+        default: {
+          type: 'money',
+          upgradeType: 'pro',
+        },
+      })
+      .toPromise();
+    if (wireEvent.type === WireEventType.Completed) {
+      this.session.getLoggedInUser().pro = true;
+      this.isPro = true;
+    }
   }
 }
