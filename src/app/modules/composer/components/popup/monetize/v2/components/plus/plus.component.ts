be4d9132de1ffaf682c05fa1bc922bb08fc5166a
Olivia Madrid <oliviajmadrid@gmail.com>
2020-06-15 21:05:21 -0600

    prepare monetization popup in post composer for stackable modal

--- src/app/modules/composer/components/popup/monetize/v2/components/plus/plus.component.ts
+++ src/app/modules/composer/components/popup/monetize/v2/components/plus/plus.component.ts
@@ -6,6 +6,7 @@ import { WireEventType } from '../../../../../../../wire/v2/wire-v2.service';
 import { Session } from '../../../../../../../../services/session';
 import { DynamicModalSettings } from '../../../../../../../../common/components/stackable-modal/stackable-modal.component';
 import { PopupService } from '../../../../popup.service';
+import { WireCreatorComponent } from '../../../../../../../wire/v2/creator/wire-creator.component';
 
 @Component({
   selector: 'm-composer__monetizeV2__plus',
@@ -20,7 +21,7 @@ export class ComposerMonetizeV2PlusComponent implements OnInit {
     private wirePaymentHandlers: WirePaymentHandlersService,
     private wireModal: WireModalService,
     private session: Session,
-    private popup: PopupService
+    private popup: PopupService // TODOOJM enable // private stackableModal: StackableModalService
   ) {}
 
   ngOnInit(): void {
@@ -31,31 +32,41 @@ export class ComposerMonetizeV2PlusComponent implements OnInit {
     this.isPro = await this.proService.isActive();
   }
 
-  // openShareModal(): void {
-  //   const componentClass = ShareModalComponent,
-  //     data = this.site.baseUrl + this.pageUrl.substr(1),
-  //     opts = { class: 'm-overlayModal__share' };
-
-  //   this.stackableModalSettings = {
-  //     componentClass: componentClass,
-  //     data: data,
-  //     opts: opts,
-  //   };
-  // }
+  async openShareModal(): Promise<void> {
+    const data = this.site.baseUrl + this.pageUrl.substr(1);
+    const opts = {
+      class: 'm-overlayModal__share m-overlay-modal--medium',
+    };
+    const stackableModalEvent: StackableModalEvent = await this.stackableModal
+      .present(ShareModalComponent, data, opts)
+      .toPromise();
+    if (stackableModalEvent.state === StackableModalState.Dismissed) {
+      console.log('*** stackableV2 dismissed');
+    }
+  }
 
   async openProUpgradeModal(): Promise<void> {
     const plusGuid = await this.wirePaymentHandlers.get('pro');
-    const wireEvent = await this.wireModal
-      .present(plusGuid, {
+    let completed = false;
+
+    const stackableModalEvent: StackableModalEvent = await this.stackableModal
+      .present(WireCreatorComponent, plusGuid, {
+        wrapperClass: 'm-modalV2__wrapper',
         default: {
           type: 'money',
-          upgradeType: 'pro',
+          upgradeType: 'plus',
+        },
+        onComplete: wire => {
+          completed = true;
+          this.isPro = true;
         },
       })
+      // TODOOJM
+      // .onDidDismiss(() => {
+      //   if (!completed) {}})})
       .toPromise();
-    if (wireEvent.type === WireEventType.Completed) {
-      this.session.getLoggedInUser().pro = true;
-      this.isPro = true;
+    if (stackableModalEvent.state === StackableModalState.Dismissed) {
+      this.isPro = completed;
     }
   }
 }
