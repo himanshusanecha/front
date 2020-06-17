b0d3acf45d2222bba18eee3ad163626121fabbba
Olivia Madrid <oliviajmadrid@gmail.com>
2020-06-17 17:03:01 -0600

    use 'custom' instead of 'ppv' for paywall badge

--- src/app/common/components/paywall-badge/paywall-badge.component.ts
+++ src/app/common/components/paywall-badge/paywall-badge.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { PaywallType } from '../../../modules/wire/lock-screen/wire-lock-screen.component';
 
 @Component({
   selector: 'm-paywallBadge',
   templateUrl: './paywall-badge.component.html',
 })
 export class PaywallBadgeComponent {
-  @Input() paywallType: 'plus' | 'tier' | 'ppv' = 'ppv';
+  @Input() paywallType: PaywallType = 'custom';
   constructor() {}
 }
