4ff2ec70f86740c97b0c3e1cf3c4b2856a850798
Olivia Madrid <oliviajmadrid@gmail.com>
2020-06-22 16:05:59 -0600

    minds+ nav sidebar item

--- src/app/modules/discovery/discovery.component.ts
+++ src/app/modules/discovery/discovery.component.ts
@@ -1,7 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'm-discovery',
   templateUrl: './discovery.component.html',
 })
-export class DiscoveryComponent {}
+export class DiscoveryComponent implements OnInit {
+  plus: boolean = false;
+  paramsSubscription: Subscription;
+
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit() {
+    /**
+     * Determines sidebar settings
+     */
+    this.paramsSubscription = this.route.params.subscribe(params => {
+      if (params['plus']) {
+        this.plus = true;
+      }
+    });
+  }
+}
